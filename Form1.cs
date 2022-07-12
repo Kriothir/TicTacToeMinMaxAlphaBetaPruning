3ebaa1b2228245eb93a3255f3af88f37c760682a
Kriothir <65174052+Kriothir@users.noreply.github.com>
2022-07-12 17:58:56 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UIVaja2
+{
+    using Player = GameTable.Players;
+
+    public partial class Form1 : Form
+    {
+
+        GameTable tictactoe;
+        int depth; 
+        // Globalne spremenljivke
+        bool turn = true;
+        int plusInf = int.MaxValue; // Infinity+ za Max
+        int minusInf = int.MinValue; // Infinity- za Min
+        public List<Button> buttonList;
+        Button selectedButton;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tictactoe = new GameTable();
+            buttonList = new List<Button>();
+            #region Buttons
+            buttonList.Add(button1); 
+            buttonList.Add(button2);
+            buttonList.Add(button3);
+            buttonList.Add(button4);
+            buttonList.Add(button5);
+            buttonList.Add(button6);
+            buttonList.Add(button7);
+            buttonList.Add(button8);
+            buttonList.Add(button9);
+            #endregion
+            depthBox.Text = "1";
+            depth = Convert.ToInt32(depthBox.Text);
+
+        }
+        private void StartRound(int column, int row)
+        {
+            // Pred pričetkom preveri ali je igre konec
+            if (tictactoe.Done())
+            {
+                checkTheWinner();
+            }
+            // Če ni, naredi potezo, kliči algoritem minimax na trenutni igralni mizi
+            else
+            {
+                tictactoe.MakeMove(column * 3 + row); // Nastavi premik igralca iz izbranega buttona
+                MinMaxAI minimax = new MinMaxAI(depth);
+                minimax.MinMax(tictactoe, Player.o, 0, minusInf, plusInf); // Klic minimax
+                moveOnBoard(tictactoe); // Premik na igralni mizi
+                if (tictactoe.Done()) // Še enkrat preverimo ali je kdo zmagal, oziroma ali je 
+                    // presegel dovoljeno število devetih premikov. Potrebno je v vsaki vejitvi preveriti drugače je treba dvakrat pritisniti po dejanskem zakljucku igre
+                {
+                    checkTheWinner();
+                }
+            }
+        }
+        private void UserMove(object sender, MouseEventArgs e)
+        {
+            selectedButton = (Button)sender;
+            // Pridobi številko gumba in nastavi pozicije za klic funkcije StartRound()
+            int column = (Int32.Parse(selectedButton.Name.Substring(selectedButton.Name.Length - 1)) - 1) / 3;
+            int row = (Int32.Parse(selectedButton.Name.Substring(selectedButton.Name.Length - 1)) - 1) % 3;
+            // Nastavimo ob vsakem kliku uporabnika na X
+            // disableamo button, da uporabnik ne more dvakrat pritisniti na isto mesto in se izognemo 
+            // neželenim efektom
+            selectedButton.Text = "X";
+            selectedButton.Enabled = false;
+            selectedButton.BackColor = Color.Yellow;
+            StartRound(column, row);
+        }
+
+      
+        private void ResetButton(object sender, EventArgs e)
+        {
+            depth = Convert.ToInt32(depthBox.Text);
+            ReinitializeBoard();
+        }
+        private void moveOnBoard(GameTable board)
+        {
+            // Pridobi poteze premika računalniškega igralca in jih postavi na gumbe
+            List<int> aiMoveList = board.AIMoves();
+            for (int i = 0; i < aiMoveList.Count; i++)
+            {
+                int position = aiMoveList[i];
+                buttonList[position].Text = "O";
+                buttonList[position].BackColor = Color.Orange;
+                buttonList[position].Enabled = false;
+            }
+        }
+        private void checkTheWinner()
+        {
+            // Preveri če je zmagovalec oziroma ali je že bilo 9 premikov in disable-a buttone
+            if (tictactoe.WinCheck() == Player.x)
+            {
+                turn = false;
+                if (!turn)
+                {
+                    foreach (Button btn in buttonList)
+                    {
+                        btn.Enabled = false;
+                    }
+                    MessageBox.Show("You win!");
+
+                }
+
+            }
+            else if (tictactoe.WinCheck() == Player.o)
+            {
+                foreach (Button btn in buttonList)
+                {
+                    btn.Enabled = false;
+                }
+                MessageBox.Show("You lost!");                
+
+            }
+            else if (tictactoe.numOfMoves() == 9 && tictactoe.WinCheck() == Player.none)
+            {
+                turn = false;
+                if (!turn)
+                {
+                    foreach (Button btn in buttonList)
+                    {
+                        btn.Enabled = false;
+                    }
+                    MessageBox.Show("Draw!");
+                }
+            }
+        }
+
+        private void ReinitializeBoard()
+        {
+            // Enabla buttone in resetira igralno mizo
+            foreach (Button btn in buttonList)
+            {
+                btn.Enabled = true;
+                btn.Enabled = true;
+                btn.BackColor = Color.White;
+                btn.Text = "";
+            }
+            tictactoe.ReinitializeGame();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MinMaxAI.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIVaja2
+{
+    using Player = GameTable.Players;
+
+    class MinMaxAI
+    {
+        // Globalne spremenljivke
+        private int depthLimit;
+        Player Tic = Player.x;
+        Player Toe = Player.o;
+        Player secondPlayer;
+        int startingIndex = -1;
+
+        public MinMaxAI(int depth)
+        {
+            depthLimit = depth; // Nastavi globino
+        }
+        // https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
+        //https://stackabuse.com/minimax-and-alpha-beta-pruning-in-python/
+        //https://iq.opengenus.org/alpha-beta-pruning-minimax-algorithm/
+        public int MinMax(GameTable board, Player firstPlay, int depth, int alfa, int beta)
+        {
+            Player currentPlayer = board.getCurrentPlayer(); // Pridobi trenutnega igralca, nasprotnega pa nastavi
+            if (firstPlay == Tic){
+                secondPlayer = Toe;
+
+            }
+            
+            secondPlayer = Tic;
+
+            if (!(board.Done() || depth == depthLimit))
+            {
+                #region Max
+                if (currentPlayer == firstPlay)  //Igralec X oziroma max na potezi
+                {
+                    int indexState = startingIndex;  // Začetni index je vedno -1, dokler ne najdemo primernega premika
+
+                    List<int> states = board.freeCoordinate(); // Pridobi prosta mesta na gameboardu
+                    int size = states.Count;
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        int newMove = states[i];
+                        GameTable newBoard = board.CurrentBoard(); // Pridobi, trenutno stanje gameboarda in naredi novo potezo
+                        newBoard.MakeMove(newMove);
+                        int currVal = MinMax(newBoard, firstPlay, depth + 1, alfa, beta); // Rekurzivni klic
+                        if (alfa >= beta) // Alfa beta izlocanje
+                        {
+                            break;
+                        }
+                        if (currVal > alfa) // Če je vrednost lista manjsa kot alfa, nastavi alfa na novo vrednost in 
+                                            // kazalec(indexstate) postavimo na vrednost pozicije premika iz states liste
+                        {
+                            alfa = currVal;
+                            indexState = newMove;
+                        }
+
+                    }
+                    if (indexState != startingIndex)  // Če kazalec indexstate ni enako -1, pomeni da smo našli premik in naredimo premik
+                    {
+                        board.MakeMove(indexState);
+                    }
+                    return alfa; // Vrnemo alfa
+                }
+                #endregion
+                #region Min
+                else
+                {
+                    int indexState = startingIndex; // Na potezi min, oziroma O
+                    List<int> states = board.freeCoordinate(); // Lista prostih mest
+                    int size = states.Count; // Velikost liste
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        int newMove = states[i]; // Premik skozi prazna mesta
+                        GameTable newBoard = board.CurrentBoard(); // Pridobimo trenutno stanje
+                        newBoard.MakeMove(newMove); // Naredimo potezo
+
+                        int currVal = MinMax(newBoard, firstPlay, depth + 1, alfa, beta);
+                        // Rekurzivni klic minimax
+                        if (beta <= alfa) // Če je alfa večji, prekini, (alfa beta izločanje)
+                        {
+                            break;
+                        }
+                        if (currVal < beta) // Isto kot pri max
+                        {
+                            beta = currVal;
+                            indexState = newMove;
+                        }
+
+                    }
+                    if (indexState != startingIndex)
+                    {
+                        board.MakeMove(indexState); // Naredimo premik, če je bil najden premik
+                    }
+                    return beta;
+                }
+                #endregion
+            }
+            else
+            {
+                return getValue(board, firstPlay); // Če je igre konec vrni rezultat (+100 ali -100)
+            }
+           
+        }
+        public int getValue(GameTable board, Player currentPlayer)
+        {
+            if(currentPlayer == Tic)
+            {
+                secondPlayer = Toe;
+            }
+            else
+            {
+                secondPlayer = Tic;
+            }
+            if (board.WinCheck() == currentPlayer && board.Done() )
+            {
+                return 100;
+            }
+            else if (board.WinCheck() == secondPlayer && board.Done())
+            {
+                return -100;
+            }
+            else
+            {
+                return board.HeuristicCalculatorForPlayer(currentPlayer);
+            }
+        }
+    }
+}
